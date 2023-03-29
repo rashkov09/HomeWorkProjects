ad69d6e1d1238ed24c21f19798aae2f0bd51eea8
dobrin_r <rashkov09@gmail.com>
2023-03-29 21:30:01 +0300

    Moved project to folder

--- src/main/java/com/lma/constants/Paths.java
+++
@@ -1,8 +0,0 @@
-package com.lma.constants;
-
-public class Paths {
-    public final static String AUTHOR_FILE_PATH = "src/authors.txt";
-    public final static String CLIENT_FILE_PATH = "src/clients.txt";
-    public final static String BOOKS_FILE_PATH = "src/books_authors_published.txt";
-    public final static String ORDERS_FILE_PATH = "src/orders.txt";
-}
