4f8a79752353068aea28b90af469e7ef4f04efb7
dobrin_r <rashkov09@gmail.com>
2023-04-30 17:29:50 +0300

    TA-21 added tests for review controller

---
+++ midterm-trippy-app/TrippyApp/src/test/java/com/scalefocus/midterm/trippyapp/testutils/Review/ReviewFactory.java
@@ -0,0 +1,26 @@
+package com.scalefocus.midterm.trippyapp.testutils.Review;
+
+import com.scalefocus.midterm.trippyapp.controller.request.ReviewRequest;
+import com.scalefocus.midterm.trippyapp.model.Review;
+import com.scalefocus.midterm.trippyapp.model.dto.ReviewDto;
+
+import static com.scalefocus.midterm.trippyapp.testutils.Review.ReviewConstants.*;
+
+public class ReviewFactory {
+
+    private ReviewFactory() {
+        throw new IllegalStateException();
+    }
+
+    public static Review getDefaultReview(){
+        return new Review(REVIEW_ID,REVIEW_USERNAME,REVIEW_RATING,REVIEW_TEXT,REVIEW_BUSINESS);
+    }
+
+    public static ReviewDto getDefaultReviewDto(){
+        return new ReviewDto(REVIEW_ID,REVIEW_USERNAME, REVIEW_STAMP_CREATED,REVIEW_RATING,REVIEW_TEXT,REVIEW_BUSINESS);
+    }
+
+    public static ReviewRequest getDefaultReviewRequest(){
+        return new ReviewRequest(REVIEW_RATING,REVIEW_TEXT,REVIEW_BUSINESS.getId());
+    }
+}
